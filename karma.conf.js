5131a18044d6252c087effe7441e6fd8516b9239
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-09-23 14:16:26 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,8 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -26,8 +28,17 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/kitchen-sink'),
       subdir: '.',
       reporters: [
+        { type: 'html' },
         { type: 'text-summary' }
       ]
-    }
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    watch: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    restartOnFileChange: false
   });
 };
